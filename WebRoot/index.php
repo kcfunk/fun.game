106dc3173261f4438b6476e06dad02afc72ed108
Zshandi Krahn <ZshandiK@gmail.com>
2017-06-22 11:44:15 -0700

    Add comments, cleanup code
    Add comments to DefaultController.php and index.php, and cleanup some of the code in index.php.

--- WebRoot/index.php
+++ WebRoot/index.php
@@ -1,4 +1,6 @@
 <?php
+//index.php will always be the first thing loaded when any page is accessed,
+// this will be ensured by the .htaccess file which I will be adding soon
 
 //Define shorthand constant for the server doc root
 define("DOCROOT", $_SERVER['DOCUMENT_ROOT']);
@@ -21,31 +23,56 @@ spl_autoload_register("load_model");
 
 //Get URI, remove "/index.php" if necessary
 $uri = str_replace("/index.php", "", $_SERVER['REQUEST_URI']);
-//Split URL into different path sections
+
+//Split URI into different path sections
+//The first section will simply be the domain name,
+// the rest is the actul path, for example:
+// "ourdomain.com/user/view/fred" will become {"ourdomain.com", "user", "view", "fred"}
 $path_array = explode("/", $uri, 4);
 
 //Init variables
 $controller = $action = $data = "";
 
-//Tracking variable
-$valid_path = false;
+//Whether the address bar contains a valid path
+$is_valid_path = false;
 
-if(isset($path_array[1]) && isset($path_array[2])){
+//Check that the path has at least the controller name and method
+if(count($path_array) >= 3){
+    //Get controller clss name from first path part,
+    // in the above example, would be "UserController"
     $controller = ucfirst($path_array[1]) . "Controller";
-    if(class_exists($controller)){
-        $action = $path_array[2];
-        if(method_exists($controller, $action)){
-            $valid_path = true;
-            if(isset($path_array[3])){
-                $data = $path_array[3];
-            }
+    //Get method to cll on the controller class, simply the second pth section,
+    // in the above example, would be "view"
+    $action = $path_array[2];
+    //Check that the specified controller and method exist
+    if( class_exists($controller) && method_exists($controller, $action) ){
+        //They exist, so the path is valid
+        $is_valid_path = true;
+        //Check if there is any extra data in the address bar
+        if(isset($path_array[3])){
+            //Get data from path
+            $data = $path_array[3];
         }
     }
 }
 
-if(!$valid_path){
+//Check if path was invalid
+if(!$is_valid_path){
+    //Invalid, so set controller to default,
+    // and default to displaying index
     $controller = "DefaultController";
     $action = "index";
 }
 
+//Call the method on the controller and pass the data as parameter
+//This is possible because PHP allows string variables to be used
+// as class and function names,
+// in the above example would resut in the following function call:
+//UserController::view("fred");
+// the default will be the following function call:
+//DefaultController::index("");
 $controller::$action($data);
+
+//Also, notice that the index method in the DefaultController does
+// not actually take any parameters, but it still gets called,
+// so PHP will simply discard the parameter when it calls the function
